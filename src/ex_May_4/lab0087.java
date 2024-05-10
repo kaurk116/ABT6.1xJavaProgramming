20d722dd8832490837a1d55bb8d73797d5a04fae
karamjeet <kaurk161@gmail.com>
2024-05-10 13:12:11 +0530

    ABT6x 1D Array in Java programming lab0087

--- src/ex_May_4/lab0087.java
+++ src/ex_May_4/lab0087.java
@@ -1,2 +1,22 @@
-package ex_May_4;public class lab0087 {
-}
+package ex_May_4;
+
+public class lab0087 {
+    public static void main(String[] args) {
+        // Program to print array  those 3(Row)*2(Column)
+        int [][] array2d={
+                {12,13},
+                {22,23},
+                {33,34}
+
+        };
+        for (int i = 0; i < array2d.length; i++) {
+            for (int j = 0; j < array2d[i].length; j++) {
+                    System.out.println(array2d[i][j]);
+
+                }
+
+            }
+
+        }
+    }
+
